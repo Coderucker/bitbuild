94d1f13ac6244bf643c2c33b68b39df4f0a7f2c8
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-12 13:34:55 +0530

    `Deps📦`: Add main file for watchman

--- internals/watchman/main.py
+++ internals/watchman/main.py
@@ -1,6 +1,2 @@
-from lib.DirectoryWatcher import DirectoryWatcher
-from lib.FileWatcher import FileWatcher
-
-watcher = FileWatcher("./tests/test.py")
-
-FileWatcher.watch(watcher)
+from internals.watchman.lib.FileWatcher import FileWatcher
+from internals.watchman.lib.DirectoryWatcher import DirectoryWatcher
