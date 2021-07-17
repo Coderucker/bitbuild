5857179856e6ca482f817220ab4dc98201037de2
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-17 11:09:44 +0530

    `Library🎓`: Add Events on File Class

---
+++ Sources/watchman/lib/events/events.py
@@ -0,0 +1,26 @@
+import os
+from typing import Callable
+from Sources.watchman.lib.events.event import Event
+
+EVENT_TYPE = [
+    "ON_CREATED",
+    "ON_DELETED",
+    "ON_MODIFIED"
+]
+
+
+class Events_File(Event):
+    def __init__(self, file) -> None:
+        self.file = file
+    
+    @staticmethod
+    def on_created(self, callback: Callable):
+        return callback()
+    
+    @staticmethod 
+    def on_modified(self, callback: Callable):
+        return callback()
+
+    @staticmethod 
+    def on_deleted(self, callback: Callable):
+        return callback()
