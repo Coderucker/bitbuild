973606b2db133891bb85273ece8893f4d82e47ce
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-17 11:09:04 +0530

    `Library🎓`: add event class

---
+++ Sources/watchman/lib/events/event.py
@@ -0,0 +1,12 @@
+class Event:
+    def __init__(self) -> None:
+        pass
+
+    def on_created(self):
+        pass
+
+    def on_modified(self):
+        pass
+    
+    def on_deleted(self):
+        pass
