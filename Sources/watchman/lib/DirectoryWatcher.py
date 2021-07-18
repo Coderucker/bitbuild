03647cbbe8f0e0f87f904f64a6d6a1b8b0f40d3f
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-18 22:27:30 +0530

    `Library🎓`: Add on_modified event to DirectoryWatcher

--- Sources/watchman/lib/DirectoryWatcher.py
+++ Sources/watchman/lib/DirectoryWatcher.py
@@ -6,25 +6,27 @@ from Sources.watchman.src.iterate import iterate
 from Sources.watchman.src.list_dir import list_dir
 
 class DirectoryWatcher:
-    def __init__(self, directory) -> None:
-        self.directory = directory
+    def __init__(self, config: list[str, str]) -> None:
+        self.config = config[0]
+        self.on_modified = config[1]
 
     def watch(self):
         current_time = useColor(datetime.now(), "255", "255", "200")
 
-        print(f"[{current_time}]: Watching out for changes in folder -> {self.directory}")
+        print(f"[{current_time}]: Watching out for changes in folder -> {self.config}")
         while(True):
             try:
-                dir_content = list_dir(self.directory)
+                dir_content = list_dir(self.config[0])
 
                 time.sleep(0.35)
 
-                if len(dir_content) != len(list_dir(self.directory)):
-                    comparison = list_dir(self.directory)
+                if len(dir_content) != len(list_dir(self.config[0])):
                     print(f"""
-[{current_time}]: New File or Folder Removed or Created!    
-{iterate(comparison)}
+[{current_time}]: New File or Folder Removed or Created! 
                     """)
+
+                    # Call on_modified event
+                    self.on_modified()
             except KeyboardInterrupt:
                 exit()
             except FileNotFoundError:
