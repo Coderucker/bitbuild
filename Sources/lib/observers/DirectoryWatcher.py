ad4476ba8ed492280ad16f4b27a4ae37e1621c33
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-19 21:43:11 +0530

    `Library🎓`: Add condition for DirectoryWatcher API

---
+++ Sources/lib/observers/DirectoryWatcher.py
@@ -0,0 +1,78 @@
+import time
+from datetime import datetime
+from typing import Union
+
+from Sources.colormania.colormania import useColor
+from Sources.src.iterate import iterate
+from Sources.src.list_dir import list_dir
+
+class DirectoryWatcher:
+    def __init__(self, config: list[str, str, str]) -> None:
+        self.config = config[0]
+        self.on_created = config[1]
+        self.on_deleted = config[2]
+        self.on_modified = config[3]
+
+    def watch(self, condition: Union[bool, int]):
+        red = "255"
+        green = "255"
+        blue = "200"
+
+        print(f"[{useColor(datetime.now(), red, green, blue)}]: Watching out for changes in folder -> {self.config}")
+
+        if type(condition) == bool:
+            while condition or True:
+                current_time = useColor(datetime.now(), red, green, blue)
+                try:
+                    dir_content = list_dir(self.config[0])
+
+                    time.sleep(0.35)
+
+                    if len(dir_content) < len(list_dir(self.config[0])):
+                        print(f"""[{current_time}]: New File Created!""")
+
+                        # Call on_created event
+                        self.on_created()
+                    elif len(dir_content) > len(list_dir(self.config[0])):
+                        print(f"""[{current_time}]: One File has been Removed!""")
+
+                        # Call deleted event
+                        self.on_deleted()
+                except KeyboardInterrupt:
+                    exit()
+                except FileNotFoundError:
+                    print(f"No file was found named: {self.config[0]}")
+                except:
+                    pass
+        else:
+            _condition = 0
+
+            while condition != _condition:
+                current_time = useColor(datetime.now(), red, green, blue)
+                try:
+                    dir_content = list_dir(self.config[0])
+
+                    time.sleep(0.35)
+
+                    if len(dir_content) < len(list_dir(self.config[0])):
+                        print(f"""[{current_time}]: New File Created!""")
+
+                        # Call on_created event
+                        self.on_created()
+
+                        # Increment Condition
+                        _condition += 1
+                    elif len(dir_content) > len(list_dir(self.config[0])):
+                        print(f"""[{current_time}]: One File has been Removed!""")
+
+                        # Call deleted event
+                        self.on_deleted()
+
+                        # Increment Condition
+                        _condition += 1
+                except KeyboardInterrupt:
+                    exit()
+                except FileNotFoundError:
+                    print(f"No file was found named: {self.config[0]}")
+                except:
+                    pass
