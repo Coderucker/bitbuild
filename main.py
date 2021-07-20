f5e27248426f318a2e230172802244dea87f643a
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-20 11:44:12 +0530

    `Integrate`: Attach with CLI

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-"""
-This is a module which is used for exporting internal API's
-"""
+import sys
 
 from Sources.main import (DirectoryWatcher, FileWatcher)
+from Sources.lib.read_config import read_config
+
+def main():
+    config = read_config()
+    config_type = config.get("type", "file")
+    config_file = config.get("targets", "main.py")[0]
+    config_on_change_command = config.get("targets", "python main.py")[1]
+    config_on_delete_command = config.get("targets", "print('deleted')")[2]
+
+    if config_type == "file":
+        watcher = FileWatcher([config_file, config_on_change_command, config_on_delete_command])
+
+        try:
+            RUN_COUNT = sys.argv.index("--count") + 1
+
+            watcher.watch(RUN_COUNT)
+
+        except ValueError:
+            watcher.watch(True)
+
+main()
