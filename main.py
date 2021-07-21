e772ce17a3e12289a9469a75f9cb7dae5b8d7905
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-21 10:53:09 +0530

    `Fix🛠️``: Run failure issue, ref: #14

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from Sources.main import (DirectoryWatcher, FileWatcher)
 from Sources.lib.read_config import read_config
 
+sys_args = sys.argv.copy()
+
 def main():
     config = read_config()
     config_type = config.get("type", "file")
@@ -10,23 +12,25 @@ def main():
     config_on_change_command = config.get("targets", "python main.py")[1]
     config_on_delete_command = config.get("targets", "print('deleted')")[2]
 
+    def get_count_index():
+        if sys_args.count('--count') > 0:
+            return sys_args.index('--count') + 1
+        else:
+            return True
+
     if config_type == "file":
         watcher = FileWatcher([config_file, config_on_change_command, config_on_delete_command])
 
-        try:
-            RUN_COUNT = 0
-            for i in range(0, len(sys.argv)):
-                if sys.argv[i] == "--count" or sys.argv[i] == "-c":
-                    RUN_COUNT = sys.argv[i + 1]
-
-            watcher.watch(RUN_COUNT)
-
-        except ValueError:
-            watcher.watch(True)
+        if sys_args.count('--count') > 0:
+            watcher.watch(get_count_index())
+        else:
+            watcher.watch(condition=True)
     elif config_type == "dir":
         watcher = DirectoryWatcher(config=[config_file, config_on_change_command, config_on_delete_command])
 
-        watcher.watch(condition=True)
+        watcher.watch(get_count_index())
+    else:
+        print(f"No such configuration: {config_type}")
 
 if __name__ == "__main__":
     main()
