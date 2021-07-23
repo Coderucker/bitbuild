11bb86fb9672884eaeaa2e914251980c232162c5
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-23 21:18:28 +0530

    Library: get unique items in a list

--- Sources/src/get_unique.py
+++ Sources/src/get_unique.py
@@ -1,8 +1,19 @@
+# Get items in a list that only appears once
 def get_unique(_list: list):
     unique_list = []
 
+    # First comparison
     for x in _list:
-        if x not in unique_list:
+        if not _list.count(x) >= 2:
             unique_list.append(x)
     
     return unique_list
+
+print(get_unique([
+    "./man",
+    "./dir",
+    "conde",
+    "ccc",
+    "conde",
+    "ccc"
+]))
