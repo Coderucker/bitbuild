e47cbf159e827b5162567c789bb3f0c204c06d0c
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-30 15:10:09 +0530

    `Library🎓`: remove .amber from CONFIG_SUPPORTED_EXTENSIONS in constants

--- BitBuild/constants.py
+++ BitBuild/constants.py
@@ -1,1 +1,1 @@
-CONFIG_SUPPORTED_EXTENSIONS = ["bitbuild.json", "bitbuild.json", ".bitbuildrc", ".bitbuild", ".amber"]
+CONFIG_SUPPORTED_EXTENSIONS = ["bitbuild.json", "bitbuild.json", ".bitbuildrc", ".bitbuild"]
