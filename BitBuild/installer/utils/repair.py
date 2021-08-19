b3608ce1046c6461b830d4987ee7a6ef499d75a3
Haneen Mahdin <haneenmahdin@gmail.com>
2021-08-19 18:44:45 +0530

    Installer: update repair function to reinstall

--- BitBuild/installer/utils/repair.py
+++ BitBuild/installer/utils/repair.py
@@ -1,13 +1,13 @@
 import os
-import sys
-from BitBuild.util.get_os import get_os
+
+from BitBuild.installer.installation import installation
 from BitBuild.util.is_in_path import is_in_path
 
-def repair():
-    env_var_path = is_in_path()
+def before_repair():
+    """
+    `Warning`: Do not import this
+    """
 
-    if env_var_path != None and os.path.exists(env_var_path):
-        print("No issues found!")
-        return True
-    else:
-        return False
+def repair():
+    # Catch Errors
+    return True if is_in_path() != None and os.path.exists(is_in_path()) else before_repair()
